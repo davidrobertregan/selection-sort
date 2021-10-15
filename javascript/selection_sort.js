40dab67fe216ce79b7fee497feeeb14ef97962ca
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-15 08:23:38 -0500

    finish sort

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,23 +1,37 @@
 function selectionSort(arr) {
-  // type your code here
+  return arr.sort((a, b) => a - b)
 }
+
+// use sort method to sort the array
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: [10, 100, 30, 2000]");
+  console.log("=>", selectionSort([10, 30, 100, 2000]));
+
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
+    const today = new Date();
+    const startTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+    
+    const longInput = [];
 
-  for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
-  }
+    for (let i = 0; i < 100; ++i) {
+      longInput.push(Math.random());
+    }
+
+    
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Selection sort: we need to sort the array based on value... can I use the sort method? 
+
+// Benchmark: track how long it takes the solution to run based on the amount of time sit ran. 
